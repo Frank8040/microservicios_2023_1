7ca7f99bfb82754c4a95ab9f82f918894b9f8829
Frank7729 <Frank 7729>
2023-05-08 15:09:43 -0500

    Completado

--- catalogo/src/main/java/com/example/catalogo/CatalogoApplication.java
+++ catalogo/src/main/java/com/example/catalogo/CatalogoApplication.java
@@ -10,18 +10,17 @@ import org.springframework.context.annotation.Bean;
 @SpringBootApplication
 public class CatalogoApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(CatalogoApplication.class, args);
-	}
+  public static void main(String[] args) {
+    SpringApplication.run(CatalogoApplication.class, args);
+  }
 
-	@Bean
-    public OpenAPI custoOpenAPI() {
-        return new OpenAPI().info(new Info()
-                .title("OPEN API MICROSERVICIOS CATALOGO")
-                .version("0.0.1")
-                .description("servicio web catalogo")
-                .termsOfService("http://swagger.io/terms")
-                .license(new License().name("Apache 2.0").url("http://springdoc.org"))
-        );
-    }
+  @Bean
+  public OpenAPI custoOpenAPI() {
+    return new OpenAPI().info(new Info()
+        .title("OPEN API MICROSERVICIOS CATALOGO")
+        .version("0.0.1")
+        .description("servicio web catalogo")
+        .termsOfService("http://swagger.io/terms")
+        .license(new License().name("Apache 2.0").url("http://springdoc.org")));
+  }
 }
