14e27faae76e3d9abac0fdd8f92c656c21b23589
Frank8040 <frank.chunca@upeu.edu.pe>
2023-05-10 19:21:36 -0500

    Completado

--- cliente/src/main/java/com/example/cliente/config/CorsConfiguration.java
+++ cliente/src/main/java/com/example/cliente/config/CorsConfiguration.java
@@ -10,7 +10,7 @@ public class CorsConfiguration implements WebMvcConfigurer {
   @Override
   public void addCorsMappings(CorsRegistry registry) {
       registry.addMapping("/**")
-              .allowedOrigins("*")
+              .allowedOrigins("http://localhost:5173")
               .allowedMethods("GET", "POST", "PUT", "DELETE")
               .allowedHeaders("*")
               .allowCredentials(true)
--- gateway-server/src/main/java/com/example/gatewayserver/GatewayServerApplication.java
+++ gateway-server/src/main/java/com/example/gatewayserver/GatewayServerApplication.java
@@ -2,11 +2,29 @@ package com.example.gatewayserver;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
 public class GatewayServerApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(GatewayServerApplication.class, args);
+	}
+
+	@Bean
+	public WebMvcConfigurer corsConfigurer() {
+		return new WebMvcConfigurer() {
+			@Override
+			public void addCorsMappings(CorsRegistry registry) {
+				registry.addMapping("/**")
+						.allowedOrigins("*")
+						.allowedMethods("GET", "POST", "PUT", "DELETE")
+						.allowedHeaders("*")
+						.allowCredentials(true)
+						.maxAge(3600);
+			}
+		};
 	}
 }
