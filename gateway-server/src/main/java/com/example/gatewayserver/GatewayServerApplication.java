a6b70086946ad93b447021d83875144d03b1c85d
Frank8040 <frank.chunca@upeu.edu.pe>
2023-05-10 20:46:50 -0500

    Completado

--- gateway-server/src/main/java/com/example/gatewayserver/GatewayServerApplication.java
+++ gateway-server/src/main/java/com/example/gatewayserver/GatewayServerApplication.java
@@ -2,29 +2,11 @@ package com.example.gatewayserver;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
-import org.springframework.web.cors.reactive.CorsWebFilter;
-import org.springframework.web.cors.reactive.UrlBasedCorsConfigurationSource;
-import org.springframework.web.cors.CorsConfiguration;
 
 @SpringBootApplication
 public class GatewayServerApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(GatewayServerApplication.class, args);
-	}
-
-	@Bean
-	public CorsWebFilter corsWebFilter() {
-		CorsConfiguration corsConfig = new CorsConfiguration();
-		corsConfig.addAllowedOriginPattern("http://localhost:5173");
-		corsConfig.addAllowedMethod("*");
-		corsConfig.addAllowedHeader("*");
-		corsConfig.setAllowCredentials(true);
-
-		UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
-		source.registerCorsConfiguration("/**", corsConfig);
-
-		return new CorsWebFilter(source);
 	}
 }
