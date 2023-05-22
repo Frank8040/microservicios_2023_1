e36959847a88a59ec862e3f89b66abb4a7d5fbed
Frank7729 <Frank 7729>
2023-05-22 11:42:20 -0500

    Completado

--- auth/src/main/java/com/example/auth/service/AuthUserService.java
+++ auth/src/main/java/com/example/auth/service/AuthUserService.java
@@ -7,7 +7,5 @@ import com.example.auth.entity.Auth;
 public interface AuthUserService {
     public Auth save(AuthUserDto dto);
     public TokenDto login(AuthUserDto dto);
-
     public TokenDto validate(String token);
-
 }
---
+++ auth/src/main/java/com/example/auth/service/impl/AuthUserServiceImpl.java
@@ -0,0 +1,57 @@
+package com.example.auth.service.impl;
+
+import com.example.auth.dto.AuthUserDto;
+import com.example.auth.dto.TokenDto;
+import com.example.auth.entity.Auth;
+import com.example.auth.repository.AuthRepository;
+import com.example.auth.security.JwtProvider;
+import com.example.auth.service.AuthUserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+
+@Service
+public class AuthUserServiceImpl implements AuthUserService {
+    @Autowired
+    AuthRepository authUserRepository;
+
+    @Autowired
+    PasswordEncoder passwordEncoder;
+
+    @Autowired
+    JwtProvider jwtProvider;
+
+    @Override
+    public Auth save(AuthUserDto dto) {
+        Optional<Auth> user = authUserRepository.findByUserName(dto.getUserName());
+        if (user.isPresent())
+            return null;
+        String password = passwordEncoder.encode(dto.getPassword());
+        Auth authUser = Auth.builder()
+                .name(dto.getUserName())
+                .password(password)
+                .build();
+        return authUserRepository.save(authUser);
+    }
+    @Override
+    public TokenDto login(AuthUserDto dto) {
+            Optional<Auth> user = authUserRepository.findByUserName(dto.getUserName());
+            if(!user.isPresent())
+                return null;
+            if(passwordEncoder.matches(dto.getPassword(), user.get().getPassword()))
+                return new TokenDto(jwtProvider.createToken(user.get()));
+            return null;
+        }
+
+    @Override
+    public TokenDto validate(String token) {
+        if(!jwtProvider.validate(token))
+            return null;
+        String username = jwtProvider.getUserNameFromToken(token);
+        if(!authUserRepository.findByUserName(username).isPresent())
+            return null;
+        return new TokenDto(token);
+    }
+}
